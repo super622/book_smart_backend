dde15ac91d1471dcc6646833aec7401f30cebb68
Senior Mobile app | Web | API Developer <seniordev52@gmail.com>
2025-05-13 06:11:18 +0300

    fix

--- app/controllers/location.controller.js
+++ app/controllers/location.controller.js
@@ -4,28 +4,30 @@ const Location = db.location;
 exports.getList = async (req, res) => {
   try {    
     const { type, user_id } = req.query;
+    console.log(type, user_id)
 
-    if (type == "Facilities") {
+    if (user_id <= 0) {
       const data = await Location.find({ locationType: type });
       return res.status(200).json({ message: "Successfully Get!", data: data });
     } else {
-      if (user_id <= 0) {
-        const data = await Location.find({ locationType: type });
+      if (type == "Hotel") {
+        const data = await Location.find({ 
+          locationType: type, 
+          userId: { $in: [parseInt(user_id), -1] } 
+        });
+        return res.status(200).json({ message: "Successfully Get!", data: data });
+      } else if (type == "Facilities") {
+        const data = await Location.find({ 
+          locationType: type, 
+          userId: { $in: [parseInt(user_id), -3] } 
+        });
         return res.status(200).json({ message: "Successfully Get!", data: data });
       } else {
-        if (type == "Hotel") {
-          const data = await Location.find({ 
-            locationType: type, 
-            userId: { $in: [parseInt(user_id), -1] } 
-          });
-          return res.status(200).json({ message: "Successfully Get!", data: data });
-        } else {
-          const data = await Location.find({ 
-            locationType: type, 
-            userId: { $in: [parseInt(user_id), -2] } 
-          });
-          return res.status(200).json({ message: "Successfully Get!", data: data });
-        }
+        const data = await Location.find({ 
+          locationType: type, 
+          userId: { $in: [parseInt(user_id), -2] } 
+        });
+        return res.status(200).json({ message: "Successfully Get!", data: data });
       }
     }
   } catch (e) {
@@ -38,54 +40,46 @@ exports.addItem = async (req, res) => {
   try {
     const { item, type, user_id } = req.body;
 
-    if (type == "Facilities") {
+    if (user_id > 0) {
+      const isExist = await Location.findOne({ locationName: item, userId: user_id });
+      const data = await Location.find({});
+  
+      if (isExist) {
+        return res.status(200).json({ message: "Already exist", data: data });
+      } else {
+        const auth = new Location({ locationName: item, locationType: type, userId: user_id });
+        await auth.save();
+        if (type == "Hotel") {
+          const data = await Location.find({ 
+            locationType: type, 
+            userId: { $in: [parseInt(user_id), -1] } 
+          });
+          return res.status(200).json({ message: "Successfully Registered", data: data });
+        } else if (type == "Facilities") {
+          const data = await Location.find({ 
+            locationType: type, 
+            userId: { $in: [parseInt(user_id), -3] } 
+          });
+          return res.status(200).json({ message: "Successfully Registered", data: data });
+        } else {
+          const data = await Location.find({ 
+            locationType: type, 
+            userId: { $in: [parseInt(user_id), -2] } 
+          });
+          return res.status(200).json({ message: "Successfully Registered", data: data });
+        }
+      } 
+    } else {
       const isExist = await Location.findOne({ locationName: item });
       const data = await Location.find({});
 
       if (isExist) {
         return res.status(200).json({ message: "Already exist", data: data });
       } else {
-        const auth = new Location({ locationName: item, locationType: type });
+        const auth = new Location({ locationName: item, locationType: type, userId: user_id });
         await auth.save();
         const newData = await Location.find({ locationType: type });
         return res.status(200).json({ message: "Successfully Registered", data: newData });
-      }
-    } else {
-      if (user_id > 0) {
-        const isExist = await Location.findOne({ locationName: item, userId: user_id });
-        const data = await Location.find({});
-    
-        if (isExist) {
-          return res.status(200).json({ message: "Already exist", data: data });
-        } else {
-          const auth = new Location({ locationName: item, locationType: type, userId: user_id });
-          await auth.save();
-          if (type == "Hotel") {
-            const data = await Location.find({ 
-              locationType: type, 
-              userId: { $in: [parseInt(user_id), -1] } 
-            });
-            return res.status(200).json({ message: "Successfully Registered", data: data });
-          } else {
-            const data = await Location.find({ 
-              locationType: type, 
-              userId: { $in: [parseInt(user_id), -2] } 
-            });
-            return res.status(200).json({ message: "Successfully Registered", data: data });
-          }
-        } 
-      } else {
-        const isExist = await Location.findOne({ locationName: item });
-        const data = await Location.find({});
-  
-        if (isExist) {
-          return res.status(200).json({ message: "Already exist", data: data });
-        } else {
-          const auth = new Location({ locationName: item, locationType: type, userId: user_id });
-          await auth.save();
-          const newData = await Location.find({ locationType: type });
-          return res.status(200).json({ message: "Successfully Registered", data: newData });
-        }
       }
     }
   } catch (e) {
