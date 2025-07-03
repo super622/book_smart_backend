6694e75100a195eeda43092a75a8258e91efa916
Senior Mobile app | Web | API Developer <techableteam@gmail.com>
2025-07-03 23:15:51 +0300

    change mongodb url

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,8 @@
 
 module.exports = {
   // url: "mongodb+srv://crystal:spskjim9939@cluster0.tod58ko.mongodb.net/"
-  url: "mongodb+srv://royhensley:qsexWNnEhwYqi3SC@cluster0.dsgesrf.mongodb.net/"
+  // url: "mongodb+srv://royhensley:qsexWNnEhwYqi3SC@cluster0.dsgesrf.mongodb.net/"
+  url: "mongodb+srv://teachableBooksmart:MiMZbwx8gCczzUca@booksmart.qihgs.mongodb.net/"
   // url: "mongodb://127.0.0.1:27017/booksmart"
   // url: process.env.ATLAS_URL || "mongodb://127.0.0.1:27017/booksmart"
 };
